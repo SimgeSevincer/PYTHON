01c84ff35846c594d5c2f24f52bc58d00128c423
Simge Sevincer <73963541+SimgeSevincer@users.noreply.github.com>
2023-01-21 20:28:15 +0300

    Add files via upload

---
+++ python projeleri/ads.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 11 16:26:03 2022
+
+@author: Lab435-Hoca
+"""
+
+"""import modula
+ 
+ print(modula.ceza)
+ 
+ yeni_borc=modula.borc
+ 
+ print(yeni_borc)"""
+ 
+ from modula import ceza, borcVer
+ 
+ print(ceza)
+ 
+ print("------------------")
+ 
+ yeni_borc2=borc_Ver(8)
+ 
+ print(yeni_borc2)
